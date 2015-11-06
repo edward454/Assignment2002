bcfbf8bbfd6f850b091e15b1ba1d69a06f1c6066
edward454 <edwardsujono@yahoo.com>
2015-11-06 19:00:50 +0800

    Update RatedMovie.java

--- Assignment/src/Movie/RatedMovie.java
+++ Assignment/src/Movie/RatedMovie.java
@@ -1,64 +1,78 @@
 package Movie;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-import Interface.Database;
-
-public class RatedMovie extends Movie implements Database{
+public class RatedMovie implements Serializable{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private String movieTitle;	
 	private double rating;
-	private boolean isRated = false;
+	private ArrayList<Movie> listMovieRated = new ArrayList<Movie>();
 	
-	public RatedMovie(double rating, String movieTitle){	
-		
-		super(movieTitle);
-		this.movieTitle = movieTitle;		
-		if(isRated()){
-			countAverageRating(rating);
+	//getter and setter for the listMovieRated
+	public void addMovieList(Movie movie){
+		//checking whether the movie has been in the list or not
+		for(int i = 0 ; i < listMovieRated.size() ; i++){
+			if(listMovieRated.get(i).getTitle().equals(movie.getTitle())){
+				return;
+			}
 		}
-		else
-			this.rating = rating;
-		isRated = true;
-	}	
-
-	
-	public double getRating(){
-		
-		return rating;
-		
+		listMovieRated.add(movie);
 	}
 	
-	public String getTitle(){
-		
-		return movieTitle;
-		
+	public ArrayList<Movie> getRatedMovie(){
+		return listMovieRated;
 	}
 	
-	public boolean isRated(){
-		
-		return isRated;
-		
+	public void removeMovie(Movie movie){
+		listMovieRated.remove(movie);
 	}
 	
-	private double countAverageRating(double rating){
-		
-		return (this.rating + rating) / 2;
-		
+	public Movie getMovieWithIndex(int index){
+		return listMovieRated.get(index);
 	}
-
-
-	@Override
-	public void writeToDatabase(String filename, ArrayList<Object> list) {
-		// TODO Auto-generated method stub
-		
+	
+	public void updateMovieRating(int index,int rating){
+		Movie movie = listMovieRated.get(index);
+		movie.setRating(rating);
+		//System.out.println("test rating: "+listMovieRated.get(index).getRating());
 	}
-
-
-	@Override
-	public ArrayList readFromDatabase(String filename) {
-		// TODO Auto-generated method stub
-		return null;
+	
+	public void updateMovieTicket(String movieName){
+		Movie bufferMovie = null;
+		for(int i = 0 ; i < listMovieRated.size() ; i++){
+			if(listMovieRated.get(i).getTitle().equals(movieName)){
+				bufferMovie = listMovieRated.get(i);
+				break;
+			}
+		}
+		if(bufferMovie!= null){
+			bufferMovie.increaseTicket();
+		}
+	}
+	
+	
+	public void printTopFiveMovieBasedOnRating(){
+		//insertion sort to sort the movie  
+		for(int i = 1 ; i < listMovieRated.size() ; i++){
+			for(int j = i ; j > 0 ;j--){
+				double first = listMovieRated.get(j).getRating();
+				double second = listMovieRated.get(j-1).getRating();
+				if(first < second){
+					//perform swap process
+					Movie buffer = listMovieRated.get(j);
+					listMovieRated.set(j, listMovieRated.get(j-1));
+					listMovieRated.set(j-1,buffer);
+				}
+			}
+		}
+		for(int i = 0 ; i< listMovieRated.size() && i <5 ;i++){
+			System.out.println(i+1+")"+"Title: "+listMovieRated.get(i).getTitle()+" Rating: "+listMovieRated.get(i).getRating());
+		}
 	}
 	
 }
